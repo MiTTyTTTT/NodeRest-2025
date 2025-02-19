f1cef34891584d40e579e2530636d8ab630a9dfb
mittytttt <s6406021622044@email.kmunb.ac.th>
2025-02-19 08:56:09 +0700

    1

---
+++ src/SequlizeSQLiteCRUDBook.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const Sequelize = require ('sequelize');
+const app =express();
+
+app.use (express.json());
+
+const sequelize = new Sequelize('database','username','password',{
+    host : 'localgost',
+    dialect : 'sqlite',
+    storage : './Database/SQBooks.sqlite'
+});
+
+const Bool = sequelize.define('book',{
+    id : {
+        type : Sequelize.INTEGER,
+        autoIncrement : true,
+        primaryKey: true
+    },
+    title{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    author: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+
+sequelize.sync();
+
+app.get('/books',(req,res) => {
+    Book.findAll(),them(Books => {
+        res.json(Books);
+    }).catch(err =>{
+        res.status(500).send(err)
+    });
+});
+
+app.get('/books/id:', (req, res ) => {
+    Book.findBypk(req.params.id).then(book =>{
+        if(!book){
+            res.status(404).send('Book not found');
+        }else{
+            res.json(book);
+        }
+    }).catch(err =>{
+        res.status(500).send(err);
+    });
+});
+
+app.post('/books', (req, res) => {
+    Book.create(req.body)
+        .then(book => {
+            res.send(book);
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
+
+app.put('/books/:id', (req, res) => {
+    Book.findByPk(req.params.id)
+        .then(book => {
+            if (!book) {
+                res.status(404).send('Book not found');
+            } else {
+                book.update(req.body)
+                    .then(() => {
+                        res.send(book);
+                    })
+                    .catch(err => {
+                        res.status(500).send(err);
+                    });
+            }
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
